a02f8d109854b13e3c6bac50220f33e250390f8b
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2022-02-17 22:39:49 +0530

    Fixed DEL endpoint and implemented PUT endpoint

--- code/app.py
+++ code/app.py
@@ -30,8 +30,21 @@ class Item(Resource):
     
     def delete(self,name):
         global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message':'Item deleted'}
+        if next(filter(lambda x: x['name']==name,items),None):
+            items = list(filter(lambda x: x['name'] != name, items))
+            return {'message':'Item deleted'}
+        return {'message':'Item not present'}, 404
+    
+    def put(self,name):
+        data =request.get_json()
+        item = next(filter(lambda x: x['name']==name,items),None)
+        if item is None:
+            item={'name':name, 'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+        
     
 class ItemList(Resource):
     def get(self):
