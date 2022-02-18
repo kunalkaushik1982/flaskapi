aa6309131b8f775303e42cb8f5b4d40021538de9
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2022-02-19 00:58:35 +0530

    Add & Retrieving Item resources from a database

--- code/create_tables.py
+++ code/create_tables.py
@@ -4,7 +4,14 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS items (pname text, price real)"
+cursor.execute(create_table)
+
+insert_item = "INSERT INTO items VALUES ('test',10.99)"
+cursor.execute(insert_item)
+
 connection.commit()
 connection.close()
