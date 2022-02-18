35a930eac2f5261df841c50ed03e405ba718ab5c
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2022-02-19 02:06:35 +0530

    Retrieving many items from the database

--- code/item.py
+++ code/item.py
@@ -1,8 +1,7 @@
 import sqlite3
+from unicodedata import name
 from flask_restful import Resource,reqparse
 from flask_jwt import jwt_required
-
-items=[]
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -96,6 +95,17 @@ class Item(Resource):
     
 class ItemList(Resource):
     def get(self):
+        return ItemList.get_all()
+    @classmethod
+    def get_all(cls):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query ="SELECT * FROM items"
+        result = cursor.execute(query)
+        items =[]
+        for row in result:
+            items.append({'name':row[0],'price':row[1]}) 
+        connection.close()
         return {'items':items}
 
         
