61e53b5a24bd516ac42d40324c0483a099440814
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2022-02-21 01:54:39 +0530

    Implemented UserModel & ItemModel using SQLAlchemy

--- section6/code/resources/item.py
+++ section6/code/resources/item.py
@@ -1,5 +1,4 @@
 import sqlite3
-from unicodedata import name
 from flask_restful import Resource,reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -24,39 +23,29 @@ class Item(Resource):
         
         item = ItemModel(name,data['price'])
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {"message":"An error occured inserting the item."},500
         return item.json(), 201
     
-
-    
     def delete(self,name):       
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query="DELETE FROM items WHERE pname=?"
-        cursor.execute(query,(name,))
-        connection.commit()
-        connection.close()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
         return {'message':'Item deleted'}
     
     def put(self,name):
-        data = Item.parser.parse_args()
-                
-        item = ItemModel.find_by_name(name)
-        updated_item=ItemModel(name, data['price'])
+        data = Item.parser.parse_args()                
+        item = ItemModel.find_by_name(name)        
         
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {'message':'An Error Occurred while inserting the data.'}, 500
+            item = ItemModel(name,data['price'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {'message':'An Error Occurred while updating the data.'},500
-        return updated_item.json()
+            item.price = data['price']
+        
+        item.save_to_db()
+        
+        return item.json()
      
 
     
--- section6/code/resources/user.py
+++ section6/code/resources/user.py
@@ -10,13 +10,8 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
-            return {"message":"A user with that name already present"},400
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()        
-        query = "INSERT INTO users VALUES (NULL,?,?)"
-        
-        cursor.execute(query,(data['username'],data['password']))        
-        connection.commit()
-        connection.close()
+            return {"message":"A user with that name already present"},400        
+        user = UserModel(**data)
+        user.save_to_db()
         
         return {"message":"User created successfully."},201
